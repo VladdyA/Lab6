79283d1b4404794b9144836178d1896ece43ce99
User <User@DESKTOP-RPSJ6FD>
2022-12-06 18:08:08 +0200

    Add project files.

---
+++ Lab6/Cat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Gender { Male, Female };
+class Cat
+{
+    private string name;
+    private double _energy;
+    public string Name { get; }
+    public Gender catGender { get; }
+
+    public double Energy
+    {
+        get { return _energy; }
+        set
+        {
+            if (value > MaxEnergy)
+            {
+                _energy = MaxEnergy;
+            }
+            else if (value < MinEnergy)
+            {
+                Console.WriteLine("Not enough energy to jump");
+            }
+            else
+            {
+                this._energy = value;
+            }
+        }
+    }
+
+    public static readonly double MaxEnergy = 20;
+    public static readonly double MinEnergy = 0;
+    public static readonly double SleepEnergyGain = 10;
+    public static readonly double JumpEnergyDrain = 0.5;
+
+    public Cat(string Name, Gender gender)
+    {
+        this.name = Name;
+        this.catGender = gender;
+        Energy = MaxEnergy;
+    }
+
+    public void Jump()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Energy -= JumpEnergyDrain;
+
+        Console.WriteLine($"{this.name} стрибнув, його енергія: {Energy}");
+    }
+
+    public void Sleep()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Energy += SleepEnergyGain;
+
+        Console.WriteLine($"{this.name} лiг спати і відновив енергію до: {Energy}");
+    }
+
+    public void CheckEnergy()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"В тебе ще {Energy} енергії");
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat("Tom", Gender.Male );
+
+
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Sleep();
+            c1.Sleep();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
